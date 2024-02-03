8ad4ea5e8028b6426685158cd326c98dd0efc9e6
xcheche <chekwubechukwu23@gmail.com>
2024-02-03 06:10:33 +0100

    in progress

---
+++ android/app/src/main/kotlin/com/example/cheche_virtual_assistant/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cheche_virtual_assistant
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:cheche_virtual_assistant/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'OpenAI ChatGpt Dale-E',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
