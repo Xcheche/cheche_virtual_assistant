bc135b44222d4882187f16ec371fa99d476ac15a
xcheche <chekwubechukwu23@gmail.com>
2024-02-04 15:52:05 +0100

    speech-to-text added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
@@ -8,6 +13,44 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   TextEditingController userInputTextEditingController =
       TextEditingController();
+  final SpeechToText speechToTextInstance = SpeechToText();
+  String recordedAudioString = "";
+  bool isLoading = false;
+
+  void initializeSpeechToText() async {
+    await speechToTextInstance.initialize();
+
+    setState(() {});
+  }
+
+  void startListeningNow() async {
+    FocusScope.of(context).unfocus();
+
+    await speechToTextInstance.listen(onResult: onSpeechToTextResult);
+
+    setState(() {});
+  }
+
+  void stopListeningNow() async {
+    await speechToTextInstance.stop();
+
+    setState(() {});
+  }
+
+  void onSpeechToTextResult(SpeechRecognitionResult recognitionResult) {
+    recordedAudioString = recognitionResult.recognizedWords;
+
+    print("Speech Result:");
+    print(recordedAudioString);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    initializeSpeechToText();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,41 +59,43 @@ class _HomeScreenState extends State<HomeScreen> {
         onPressed: () {},
         child: Padding(
           padding: const EdgeInsets.all(4.0),
-          child: Image.asset('images/sound.png'),
+          child: Image.asset("images/sound.png"),
         ),
       ),
       appBar: AppBar(
         flexibleSpace: Container(
           decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.purpleAccent.shade100,
-                Colors.deepPurple,
-              ],
-            ),
-          ),
+              gradient: LinearGradient(colors: [
+            Colors.purpleAccent.shade100,
+            Colors.deepPurple,
+          ])),
         ),
         title: Image.asset(
-          'images/logo.png',
+          "images/logo.png",
           width: 140,
         ),
         titleSpacing: 10,
         elevation: 2,
-        actions: const [
+        actions: [
+          //chat
           Padding(
-            padding: EdgeInsets.only(right: 4, top: 4),
+            padding: const EdgeInsets.only(right: 4, top: 4),
             child: InkWell(
-              child: Icon(
+              onTap: () {},
+              child: const Icon(
                 Icons.chat,
                 size: 40,
                 color: Colors.white,
               ),
             ),
           ),
+
+          //image
           Padding(
-            padding: EdgeInsets.only(right: 8, left: 4),
+            padding: const EdgeInsets.only(right: 8, left: 4),
             child: InkWell(
-              child: Icon(
+              onTap: () {},
+              child: const Icon(
                 Icons.image,
                 size: 40,
                 color: Colors.white,
@@ -64,46 +109,75 @@ class _HomeScreenState extends State<HomeScreen> {
           padding: const EdgeInsets.all(20),
           child: Column(
             children: [
+              const SizedBox(
+                height: 40,
+              ),
+
+              //image
               Center(
                 child: InkWell(
-                  onTap: () {},
-                  child: Image.asset(
-                    'images/assistant_icon.png',
-                    height: 300,
-                  ),
+                  onTap: () {
+                    speechToTextInstance.isListening
+                        ? stopListeningNow()
+                        : startListeningNow();
+                  },
+                  child: speechToTextInstance.isListening
+                      ? Center(
+                          child: LoadingAnimationWidget.beat(
+                            size: 300,
+                            color: speechToTextInstance.isListening
+                                ? Colors.deepPurple
+                                : isLoading
+                                    ? Colors.deepPurple[400]!
+                                    : Colors.deepPurple[200]!,
+                          ),
+                        )
+                      : Image.asset(
+                          "images/assistant_icon.png",
+                          height: 300,
+                          width: 300,
+                        ),
                 ),
               ),
+
               const SizedBox(
                 height: 50,
               ),
+
+              //text field with a button
               Row(
                 children: [
+                  //text field
                   Expanded(
                     child: Padding(
-                      padding: EdgeInsets.only(left: 4.0),
+                      padding: const EdgeInsets.only(left: 4.0),
                       child: TextField(
                         controller: userInputTextEditingController,
                         decoration: const InputDecoration(
                           border: OutlineInputBorder(),
-                          labelText: 'How can i help you?',
+                          labelText: "how can i help you?",
                         ),
                       ),
                     ),
                   ),
+
                   const SizedBox(
-                    height: 10,
+                    width: 10,
                   ),
+
+                  //button
                   InkWell(
                     onTap: () {
-                      print('Send');
+                      print("send user input");
                     },
                     child: AnimatedContainer(
                       padding: const EdgeInsets.all(15),
                       decoration: const BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        color: Colors.deepPurpleAccent,
+                          shape: BoxShape.rectangle,
+                          color: Colors.deepPurpleAccent),
+                      duration: const Duration(
+                        milliseconds: 1000,
                       ),
-                      duration: const Duration(milliseconds: 1000),
                       curve: Curves.bounceInOut,
                       child: const Icon(
                         Icons.send,
